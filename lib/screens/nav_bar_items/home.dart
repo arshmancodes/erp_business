8571e799839653d437b88221554a451250a8602e
arshmancodes <arshman3@gmail.com>
2021-09-26 18:17:06 +0500

    Updates

--- lib/screens/nav_bar_items/home.dart
+++ lib/screens/nav_bar_items/home.dart
@@ -1,4 +1,7 @@
+import 'package:erp_business/controllers/attendanceController.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +11,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final attendanceController = Get.find<AttendanceController>();
+
+  void getAttendance() async {
+    http.Response response = await attendanceController.getAttendance();
+  }
+
+  @override
+  void initState() {
+    var list = getAttendance();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +30,7 @@ class _HomeState extends State<Home> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Home Screen is Under Development"),
-            ],
+            children: [],
           ),
         ),
       ),
