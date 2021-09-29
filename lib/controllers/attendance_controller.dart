98b447602fef5d73e9fcdbe0ae94b1e0f3c54866
arshmancodes <arshman3@gmail.com>
2021-09-30 03:15:23 +0500

    Completed QR Build and AdminDashboard

--- lib/controllers/attendance_controller.dart
+++ lib/controllers/attendance_controller.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class AttendanceController extends GetxController {
-  late List<AttendanceModel> attendance;
+  late List<AttendanceModel> attendance = [];
 
   Future<http.Response> getAttendance() async {
     var url = Uri.parse('https://10.0.2.2:3000/api/attendance/1');
@@ -15,6 +15,16 @@ class AttendanceController extends GetxController {
       return response;
     } on Exception catch (e) {
       rethrow;
+    }
+  }
+
+  void attendanceModel() {
+    for (int i = 0; i < 5; i++) {
+      AttendanceModel model = AttendanceModel(
+          id: i, userid: i * 3, attendance: true, date: "24 Sept");
+      print("Attendance model ran ${i}");
+
+      attendance.add(model);
     }
   }
 }
