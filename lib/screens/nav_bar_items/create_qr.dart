f3f1c5d702390d6fa5d585055ab87f5b39debb0b
arshmancodes <arshman3@gmail.com>
2021-09-29 23:17:15 +0500

    Updated

---
+++ lib/screens/nav_bar_items/create_qr.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CreateQR extends StatefulWidget {
+  const CreateQR({Key? key}) : super(key: key);
+
+  @override
+  _CreateQRState createState() => _CreateQRState();
+}
+
+class _CreateQRState extends State<CreateQR> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
